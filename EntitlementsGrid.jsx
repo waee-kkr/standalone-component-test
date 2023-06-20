862f75f93d55834234e4fe1d14fa20f38de967fd
William Espinoza <espiw@kkr.com>
2023-06-20 12:24:50 -0600

    testin remote component

--- EntitlementsGrid.jsx
+++ EntitlementsGrid.jsx
@@ -1,160 +1,85 @@
-(function(e, a) { for(var i in a) e[i] = a[i]; }(exports, /******/ (function(modules) { // webpackBootstrap
-  /******/ 	// The module cache
-  /******/ 	var installedModules = {};
-  /******/
-  /******/ 	// The require function
-  /******/ 	function __webpack_require__(moduleId) {
-  /******/
-  /******/ 		// Check if module is in cache
-  /******/ 		if(installedModules[moduleId]) {
-  /******/ 			return installedModules[moduleId].exports;
-  /******/ 		}
-  /******/ 		// Create a new module (and put it into the cache)
-  /******/ 		var module = installedModules[moduleId] = {
-  /******/ 			i: moduleId,
-  /******/ 			l: false,
-  /******/ 			exports: {}
-  /******/ 		};
-  /******/
-  /******/ 		// Execute the module function
-  /******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
-  /******/
-  /******/ 		// Flag the module as loaded
-  /******/ 		module.l = true;
-  /******/
-  /******/ 		// Return the exports of the module
-  /******/ 		return module.exports;
-  /******/ 	}
-  /******/
-  /******/
-  /******/ 	// expose the modules object (__webpack_modules__)
-  /******/ 	__webpack_require__.m = modules;
-  /******/
-  /******/ 	// expose the module cache
-  /******/ 	__webpack_require__.c = installedModules;
-  /******/
-  /******/ 	// define getter function for harmony exports
-  /******/ 	__webpack_require__.d = function(exports, name, getter) {
-  /******/ 		if(!__webpack_require__.o(exports, name)) {
-  /******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
-  /******/ 		}
-  /******/ 	};
-  /******/
-  /******/ 	// define __esModule on exports
-  /******/ 	__webpack_require__.r = function(exports) {
-  /******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-  /******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-  /******/ 		}
-  /******/ 		Object.defineProperty(exports, '__esModule', { value: true });
-  /******/ 	};
-  /******/
-  /******/ 	// create a fake namespace object
-  /******/ 	// mode & 1: value is a module id, require it
-  /******/ 	// mode & 2: merge all properties of value into the ns
-  /******/ 	// mode & 4: return value when already ns object
-  /******/ 	// mode & 8|1: behave like require
-  /******/ 	__webpack_require__.t = function(value, mode) {
-  /******/ 		if(mode & 1) value = __webpack_require__(value);
-  /******/ 		if(mode & 8) return value;
-  /******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
-  /******/ 		var ns = Object.create(null);
-  /******/ 		__webpack_require__.r(ns);
-  /******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
-  /******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
-  /******/ 		return ns;
-  /******/ 	};
-  /******/
-  /******/ 	// getDefaultExport function for compatibility with non-harmony modules
-  /******/ 	__webpack_require__.n = function(module) {
-  /******/ 		var getter = module && module.__esModule ?
-  /******/ 			function getDefault() { return module['default']; } :
-  /******/ 			function getModuleExports() { return module; };
-  /******/ 		__webpack_require__.d(getter, 'a', getter);
-  /******/ 		return getter;
-  /******/ 	};
-  /******/
-  /******/ 	// Object.prototype.hasOwnProperty.call
-  /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
-  /******/
-  /******/ 	// __webpack_public_path__
-  /******/ 	__webpack_require__.p = "";
-  /******/
-  /******/
-  /******/ 	// Load entry module and return exports
-  /******/ 	return __webpack_require__(__webpack_require__.s = 1);
-  /******/ })
-  /************************************************************************/
-  /******/ ([
-  /* 0 */
-  /***/ (function(module, exports) {
-  
-  module.exports = require("react");
-  
-  /***/ }),
-  /* 1 */
-  /***/ (function(module, __webpack_exports__, __webpack_require__) {
-  
-  "use strict";
-  __webpack_require__.r(__webpack_exports__);
-  /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
-  /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-  function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest(); }
-  
-  function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance"); }
-  
-  function _iterableToArrayLimit(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
-  
-  function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
-  
-  /**
-   * Entrypoint of the Remote Component.
-   */
-  
-  
-  var Counter = function Counter() {
-    var _useState = Object(react__WEBPACK_IMPORTED_MODULE_0__["useState"])(0),
-        _useState2 = _slicedToArray(_useState, 2),
-        count = _useState2[0],
-        setCount = _useState2[1];
-  
-    var increase = function increase() {
-      return setCount(count + 1);
-    };
-  
-    var decrease = function decrease() {
-      return setCount(count - 1);
-    };
-  
-    return (<div style={{
-      height: 'inherit'
-    }} id ='test'>
-        <div
-        className="ag-theme-alpine auto-margin"
-        style={{
-          height: 'inherit'
-        }}
-      >
-        
-        <AgGridReact
-            columnDefs={gridProps['columnDefs']}
-            rowBuffer={0}
-            rowData={data}
-            domLayout={'normal'}
-            pagination={false}
-            treeData={true}
-            alwaysShowHorizontalScroll={true}
-            autoGroupColumnDef={auto_group_col}
-            getDataPath={getDataPath()}>
-              
-            
+import React from 'react';
+import '../App.css';
+import "../s1.css"
+import '../overwrites.css'
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css';
+import 'ag-grid-community/dist/styles/ag-theme-alpine.css';
+import axios from 'axios';
+import {useState} from 'react';
+import {LicenseManager} from "ag-grid-enterprise";
+LicenseManager.setLicenseKey("CompanyName=KKR,LicensedGroup=KKR Credit Tech 1,LicenseType=MultipleApplications,LicensedConcurrentDeveloperCount=1,LicensedProductionInstancesCount=0,AssetReference=AG-008671,ExpiryDate=24_June_2021_[v2]_MTYyNDQ4OTIwMDAwMA==9ba2fe4c126a19353c97ffad5cdbf8dc");
 
 
-                    </AgGridReact>
-                    </div>
-</div>);
+import { useQuery , QueryClient, QueryClientProvider  } from '@tanstack/react-query';
+
+function EntitlementsGrid (props) {
+  return(
+  <QueryClientProvider client={queryClient}>
+    <EntitlementsGrid2 props={props}/>
+  </QueryClientProvider>
+  )
+}
+
+const queryClient = new QueryClient();
+
+const getDataPath = () => {
+  return (data) => {
+    return data.Parent;
   };
+};
+
+function EntitlementsGrid2(props) {
+
+  const gridProps=props['props']
+  const data=gridProps['data']
+  const auto_group_col=gridProps['auto_group_col']
   
-  /* harmony default export */ __webpack_exports__["default"] = (Counter);
+  const [content, setValue] = useState('main');
+  const [rowData, setRowData] = useState([]);
+
+
   
-  /***/ })
-  /******/ ])));
+  return (  
+    <div style={{
+      height: 'inherit'
+    }}>
+
+          {content ? (
+                      <div style={{
+                        height: 'inherit'
+                      }} id ='test'>
+                          <div
+                          className="ag-theme-alpine auto-margin"
+                          style={{
+                            height: 'inherit'
+                          }}
+                        >
+                          
+                          <AgGridReact
+                              columnDefs={gridProps['columnDefs']}
+                              rowBuffer={0}
+                              rowData={data}
+                              domLayout={'normal'}
+                              pagination={false}
+                              treeData={true}
+                              alwaysShowHorizontalScroll={true}
+                              autoGroupColumnDef={auto_group_col}
+                              getDataPath={getDataPath()}>
+                                
+                              
+              
+              
+                                      </AgGridReact>
+                                      </div>
+                  </div>
+                    )
+                  : <div>beep</div>    
+}
+
+      </div>
+    
+  );
+}
+
+export default EntitlementsGrid;
